ab6ae2237637b146fbb1357d37bbea3fac4bf917
Arnold <antaloczi.arnold@gmail.com>
2021-03-26 20:03:12 +0100

    Improved film cards

--- src/app/widget/film-card/film-card.component.ts
+++ src/app/widget/film-card/film-card.component.ts
@@ -10,10 +10,16 @@ import { Film } from 'src/app/model/film';
 export class FilmCardComponent implements OnInit {
 
   @Input() film = new Film();
+  starsArray: any[] = [];
+  genreArray: string[] = [];
+  genres: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    this.starsArray = new Array(this.film.stars);
+    this.genreArray = this.film.genres;
+    this.genres = this.genreArray.join(", ");
   }
 
 }
