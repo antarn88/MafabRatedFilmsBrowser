d04346c2dd255d81ea29646191e47c0124c14d44
Arnold <antaloczi.arnold@gmail.com>
2023-05-12 13:52:27 +0200

    Change tooltip lib, newer fork

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { TooltipModule } from 'ng2-tooltip-directive';
+import { TooltipModule } from 'ng2-tooltip-directive-ngfix';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
